4c0c73d48a815cff18475951698b3e2f02a5fe33
pvulnguyen <pvulnguyen@gmail.com>
2024-01-17 12:42:49 -0800

    add remote schema

---
+++ supabase/migrations/20240117151644_remote_schema.sql
@@ -0,0 +1,16 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+  begin
+    insert into public.users (account_id, onboarded, deactivated)
+    values (new.id, false, false);
+    return new;
+  end;
+$function$
+;
+
+
