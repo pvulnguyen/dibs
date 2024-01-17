13ca8bf06909c5610c7dc93b3abc86e9fdadde6d
pvulnguyen <pvulnguyen@gmail.com>
2024-01-16 20:42:38 -0800

    init supabase

---
+++ supabase/migrations/20240117043004_create_users_table.sql
@@ -0,0 +1,24 @@
+create table users (
+  id uuid default gen_random_uuid() primary key,
+  account_id uuid references auth.users(id) on delete set null,
+  onboarded boolean not null default false,
+  deactivated boolean not null default false
+);
+
+alter table users enable row level security;
+
+create policy read_own on users for select to authenticated using (auth.uid() = account_id);
+create policy insert_own on users for insert to authenticated with check (auth.uid() = account_id);
+create policy update_own on users for update to authenticated using (auth.uid() = account_id);
+
+create function public.handle_new_user() returns trigger as $$
+  begin
+    insert into users (account_id, onboarded, deactivated)
+    values (new.id, false, false);
+    return new;
+  end;
+$$ language plpgsql security definer;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
